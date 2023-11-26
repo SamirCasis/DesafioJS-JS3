b77ceb891a7b28d9310da6f1b1d79dee224c2eab
the_p <the_punisher_deep@hotmail.com>
2023-11-26 07:21:18 -0300

    iniciando desafio 3 js, se agregan html's y js's

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,9 @@
+/* ejemplo F declarada
+function example(a, b, c){
+return a+b+c
+}
+ */
+
+// F expresada
+
+example = function (suma) { a + b + c };
---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+/* function pintar(){
+    ele.style.backgroundColor = 'yellow'
+    }
+    const ele = document.getElementById("ele1")
+ele.addEventListener("click", pintar); */
+
+
+const ele1 = document.getElementById('ele1');
+const pintar = (ele1, color = 'green')
+
+ele1.addEventListener = ('click', (pintar) => {
+    ele1.style.color = 'yellow'
+    return
+})
+
+cambiaColor =
+
+    cambiaColor1 = ('click', () => {
+        const d1 = document.getElementById('d1')
+        d1.style.backgroundColor = 'black'
+        return
+    })
+
+
